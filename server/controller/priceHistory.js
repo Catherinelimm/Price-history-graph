ee13da1b73bb08ce3500733f100eb50caf9e3523
Catherinelimm <124689577+Catherinelimm@users.noreply.github.com>
2024-03-04 23:25:52 +0000

    when the api is not working, it will also show the price history graph

--- server/controller/priceHistory.js
+++ server/controller/priceHistory.js
@@ -4,32 +4,40 @@ const PriceHistory = require("../model/priceHistory");
 const axios = require("axios");
 const { priceByDomain } = require("../helper/priceByDomain");
 
-router.post("/price-history", async (req, res) => {
-  try {
-    const { itemName, country, source } = req.body;
 
-    let result;
+router.post(
+  "/price-history",async (req, res, next) => {
+    try {
+      const { itemName, country, source } = req.body;
 
-    if (source === "Amazon" || source === "Ebay") {
-      result = await fetchPriceData(source, itemName, country);
-      await saveToDatabase(result, country, source);
+      let result, retrievedData;
+
+      if (source === "Amazon" || source === "Ebay") {
+        try {
+          result = await fetchPriceData(source, itemName, country);
+          await saveToDatabase(result, country, source);
+        } catch (fetchError) {
+          console.error(fetchError);
+        }
+      }
+
+      // Move this outside of the inner try-catch block
+      retrievedData = await retrievePriceHistoryFromDatabase(
+        itemName,
+        country,
+        source
+      );
+
+      res.status(200).json({
+        message: "Data saved and retrieved successfully",
+        data: retrievedData,
+      });
+    } catch (error) {
+      console.error("Error processing request:", error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
-
-    const retrievedData = await retrievePriceHistoryFromDatabase(
-      itemName,
-      country,
-      source
-    );
-
-    res.status(200).json({
-      message: "Data saved and retrieved successfully",
-      data: retrievedData,
-    });
-  } catch (error) {
-    console.error("Error processing request:", error);
-    res.status(500).json({ error: "Internal Server Error" });
   }
-});
+);
 
 async function saveToDatabase(products, country, source) {
   const dateCreated = new Date().toLocaleDateString("en-GB");
@@ -39,14 +47,12 @@ async function saveToDatabase(products, country, source) {
     UK: "GBP",
   };
 
-  // console.log(products.search_results.length, "Products");
-
   const dataFromSource = {
     Amazon: products?.data?.products,
     Ebay: products?.search_results,
   };
 
-  console.log(dataFromSource[source], "Data from source");
+  // console.log(dataFromSource[source], "Data from source");
 
   const saveDbObj = {
     currency: currencyObj[country],
@@ -100,7 +106,7 @@ async function fetchPriceData(source, itemName, country) {
 
     return response.data;
   } catch (error) {
-    console.error(error);
+    // console.error(error);
     throw new Error(`Error in Fetch ${source} Price Data function`);
   }
 }
@@ -114,10 +120,10 @@ async function retrievePriceHistoryFromDatabase(itemName, country, source) {
     };
     const priceHistoryData = await PriceHistory.find(query);
 
-    console.log(priceHistoryData, "Price History Data");
+    // console.log(priceHistoryData, "Price History Data");
     return priceHistoryData;
   } catch (error) {
-    console.error("Error retrieving data from Database:", error);
+    // console.error("Error retrieving data from Database:", error);
     throw new Error("Error in Retrieve Price History from Database function");
   }
 }
